51f94b2d299f56728778c2bd798a945cad678fba
Aditya S Kelaskar <kelaskaraditya1@gmail.com>
2025-01-21 18:48:28 +0530

    Initial commit

---
+++ src/test/java/com/StarkIndustries/SpringBootFileUploading/SpringBootFileUploadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StarkIndustries.SpringBootFileUploading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFileUploadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
